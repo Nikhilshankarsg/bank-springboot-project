2b921bbe367078c97f8b07aebfd2e72cb9ec1f22
Nikhil27-web <nikhilshankarsg@gmail.com>
2024-10-04 20:30:52 +0530

    Hello World

---
+++ accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
@@ -0,0 +1,10 @@
+package com.easybytes.accounts;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hai");
+    }
+}
