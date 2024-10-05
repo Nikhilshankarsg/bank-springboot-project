dfa13c37370f166321312524cadd0919c88c0a70
Nikhil27-web <nikhilshankarsg@gmail.com>
2024-10-05 19:40:01 +0530

    basic

---
+++ src/main/java/com/demo/bank_project/DemoBankApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bank_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/bank_project/DemoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bank_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
