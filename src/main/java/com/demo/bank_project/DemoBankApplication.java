36eaf103daf8c96c6e107193f98a20b11e6ee5ca
Nikhil27-web <nikhilshankarsg@gmail.com>
2024-10-05 21:18:50 +0530

    basic

--- src/main/java/com/demo/bank_project/DemoBankApplication.java
+++ src/main/java/com/demo/bank_project/DemoBankApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoBankApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hai");
 		SpringApplication.run(DemoBankApplication.class, args);
 	}
 
